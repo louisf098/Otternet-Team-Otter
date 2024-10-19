ab4e0b1e5f29cc24349a3a65ed79a06a1166ce16
Daniel Leong <daniel.leong@stonybrook.edu>
2024-10-19 12:48:27 -0400

    Added connected proxy node msg

--- frontend/src/ui/pages/Proxy.tsx
+++ frontend/src/ui/pages/Proxy.tsx
@@ -1,8 +1,12 @@
 import ProxyPref from "../components/ProxyPref";
 import NodesSection from "../components/NodesSection";
 import { Box, Typography } from "@mui/material";
+import { useContext } from "react";
+import { ProxyContext } from "../contexts/ProxyContext";
 
 const Proxy = () => {
+  const { selectedNode } = useContext(ProxyContext);
+
   return (
     <Box sx={{ m: 4, width: "85%" }}>
       <Typography variant="h4" sx={{ textAlign: "left" }}>
@@ -12,6 +16,14 @@ const Proxy = () => {
       <Typography variant="h4" sx={{ textAlign: "left" }}>
         Select a Proxy Node
       </Typography>
+      {selectedNode ? (
+        <Typography variant="h6" sx={{ textAlign: "center" }}>
+          Connected to Proxy Node ID:{" "}
+          <span style={{ fontWeight: "bold", color: "blue" }}>
+            {selectedNode.id}
+          </span>
+        </Typography>
+      ) : null}
       <NodesSection />
     </Box>
   );
