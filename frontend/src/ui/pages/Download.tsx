a003b7c58b2da8c8ac33d666fa511a18e840505f
AMuiSBU <andrew.mui.1@stonybrook.edu>
2024-10-20 21:23:42 -0400

    Refactor to use market page download modal

--- frontend/src/ui/pages/Download.tsx
+++ frontend/src/ui/pages/Download.tsx
@@ -1,7 +1,7 @@
 import { Box } from "@mui/material";
-import {TextField} from "@mui/material";
-import {Typography, Card, Grid, CardContent, CardActions} from "@mui/material";
-import {Button, CircularProgress, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions} from "@mui/material";
+import { TextField } from "@mui/material";
+import { Typography, Card, Grid, CardContent, CardActions, Modal } from "@mui/material";
+import { Button, CircularProgress } from "@mui/material";
 import { useState } from "react";
 import VerifiedUserIcon from '@mui/icons-material/VerifiedUser';
 
@@ -21,106 +21,125 @@ interface FormData {
 const dummyProviders = [
   {walletID: 'e0d123e5f316bef78bfdf5a008837577', price: 12}, 
   {walletID: '95982461e7db28fb0d0ea25bd2fc9d7f', price: 15}, 
-  {walletID: '06adac43bac94634b3773f13296cf6d9', price: 19}]
+  {walletID: '06adac43bac94634b3773f13296cf6d9', price: 19}
+];
 
-  const dummyFileDetails = {
-    name: "Cookie_Monster_Script",
-    type: "Text Document(.txt)",
-    size: 174
-  };
+const dummyFileDetails = {
+  name: "Cookie_Monster_Script.txt",
+  type: "Text Document(.txt)",
+  size: 174
+};
 
 const Download = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [downloadModalOpen, setDownloadModalOpen] = useState<boolean>(false);
+  const [downloadLocation, setDownloadLocation] = useState<string>("");
+  const [fileHash, setFileHash] = useState("");
+  const [searchedHash, setSearchedHash] = useState("");
+  const [providers, setProviders] = useState<{walletID: string; price: number;}[]>([]);
+  const [fileDetails, setFileDetails] = useState<{name: string; type: string; size: number;} | null >(null);
+  const [selectedPrice, setSelectedPrice] = useState<number>();
+  const [selectedWallet, setSelectedWallet] = useState<string>("");
 
-    const [isLoading, setIsLoading] = useState(false); // Track loading state
-    const [openModal, setOpenModal] = useState(false);  // Track modal visibility
-    const [modalMessage, setModalMessage] = useState("");  // Set Modal Message
-    const [modalTitle, setModalTitle] = useState("");  // Set Modal Title
-    const [fileHash, setFileHash] = useState(""); // Capture file hash from textfield
-    const [searchedHash, setSearchedHash] = useState(""); // Store file hash from textfield
-    const [providers, setProviders] = useState<{walletID: string; price: number;}[]>([]);
-    const [fileDetails, setFileDetails] = useState<{name: string; type: string; size: number;} | null >(null);
+  const handleSearchClick = () => {
+    setIsLoading(true);
 
-    const handleSearchClick = () => {
-      setIsLoading(true)
+    setTimeout(() => {
+      setProviders(dummyProviders);
+      setSearchedHash(fileHash);
+      setFileDetails(dummyFileDetails);
+      setIsLoading(false);
+    }, 2000);
+  }
 
-      setTimeout(() => {
-        setProviders(dummyProviders);
-        setSearchedHash(fileHash);
-        setFileDetails(dummyFileDetails);
-        setIsLoading(false);
-      }, 2000);
+  const handleDownloadClick = async (phash: string, pprice: number) => {
+    try {
+      const postData: FormData = {
+        userID: phash, 
+        price: pprice,
+        fileName: dummyFileDetails.name,
+        filePath: downloadLocation,
+        fileSize: dummyFileDetails.size,
+        fileType: dummyFileDetails.type,
+        timestamp: new Date().toISOString(),
+        fileHash: searchedHash,
+        bundleMode: false
+      };
+      
+      const response = await fetch("http://localhost:9378/download", {
+        method: 'POST',
+        body: JSON.stringify(postData),
+      });
+      
+      console.log("Response: ", response);
+      setDownloadModalOpen(false);
+      setDownloadLocation("");
+
+    } catch (err: any) {
+      console.error("An error occurred during download:", err);
+      setDownloadModalOpen(false);
+      setDownloadLocation("");
     }
+  };
 
-    const handleDownloadClick = async (phash: string, pprice: number) => {
-      try {
-        const postData: FormData = {
-          userID: phash, 
-          price: pprice,
-          fileName: dummyFileDetails.name,
-          filePath: "/user/Downloads",  // This can be modified to actual download path
-          fileSize: dummyFileDetails.size,
-          fileType: dummyFileDetails.type,
-          timestamp: new Date().toISOString(),
-          fileHash: searchedHash,
-          bundleMode: false
-        };
-        
-        const response = await fetch("http://localhost:9378/download", {
-          method: 'POST',
-          body: JSON.stringify(postData),
-        });
-        
-        console.log("Response: ", response);
-        setModalMessage("Download should begin shortly...");
-        setModalTitle("Transaction successful!");
-        setOpenModal(true);
-  
-      } catch (err: any) {
-        console.error("An error occurred during download:", err);
-        setModalMessage(`Download failed: ${err.message}`);
-        setModalTitle("Error");
-        setOpenModal(true);
+  const handleDownload = (walletid: string, price: number) => {
+    setSelectedWallet(walletid);
+    setSelectedPrice(price);
+    setDownloadModalOpen(true);
+  };
+
+  const handleSelectDownloadLocation = async () => {
+    try {
+      if (!window.electronAPI || !window.electronAPI.selectDownloadPath) {
+        throw new Error("Electron API not available");
       }
-    };
-
-    const handleCloseModal = () => {
-      setOpenModal(false);
+      const location = await window.electronAPI.selectDownloadPath();
+      if (!location) {
+        throw new Error("Problem with selecting download location");
+      }
+      setDownloadLocation(location);
+    } catch (err: any) {
+      console.error("Error with selecting download location: ", err);
     }
+  };
 
-    return (
-        <Box
-        sx={{
-          width: '100%',             
-          maxWidth: '1000px',         
-          m: '0 auto',               
-          p: 2,                      
-          textAlign: 'center', 
-          }}
-        >
-        <Box sx={{ m: 1, p: 2, width: '100%', maxWidth: '1000px' }}>  {/* Adjust form size here */}
+  const handleDownloadModalClose = () => {
+    setDownloadModalOpen(false);
+    setDownloadLocation("");
+  };
+
+  return (
+    <Box sx={{
+      width: '100%',             
+      maxWidth: '1000px',         
+      m: '0 auto',               
+      p: 2,                      
+      textAlign: 'center', 
+    }}>
+      <Box sx={{ m: 1, p: 2, width: '100%', maxWidth: '1000px' }}>
         <Typography variant="h3" sx={{ mb: 2, textAlign: 'center' }}>
           Download File
         </Typography>
-          <TextField
-            id="outlined-hash"
-            label="Input File Hash Here"
-            variant="outlined"
-            value = {fileHash}
-            onChange = {(e) => setFileHash(e.target.value)}
-            fullWidth
-            sx={{ mb: 2 }}
-          />
+        <TextField
+          id="outlined-hash"
+          label="Input File Hash Here"
+          variant="outlined"
+          value={fileHash}
+          onChange={(e) => setFileHash(e.target.value)}
+          fullWidth
+          sx={{ mb: 2 }}
+        />
 
-        <Button variant="contained" sx={{ mt: 2 }} type="submit" onClick={handleSearchClick}
-            disabled={isLoading}>
-                {isLoading ? (
-            <CircularProgress size={24} /> 
-          ) : (
-            "Search Providers"
-          )}
+        <Button 
+          variant="contained" 
+          sx={{ mt: 2 }} 
+          type="submit" 
+          onClick={handleSearchClick}
+          disabled={isLoading}
+        >
+          {isLoading ? <CircularProgress size={24} /> : "Search Providers"}
         </Button>
 
-        {/* Display file details if they exist */}
         {fileDetails && (
           <Box sx={{ mt: 2, textAlign: 'left' }}>
             <Box sx={{ display: 'inline-flex', alignItems: 'center' }}>
@@ -137,59 +156,87 @@ const Download = () => {
         )}
 
         <Box sx={{ mt: 2 }}>
-        <Grid container spacing={2}>
-          {providers.map((provider) => (
+          <Grid container spacing={2}>
+            {providers.map((provider) => (
               <Grid item xs={12} sm={6} key={provider.walletID}>
                 <Card>
                   <CardContent>
                     <Box display="flex" justifyContent="space-between">
-                    <Box display="flex" flexDirection="column" alignItems="flex-start">
-                    <Typography variant="body2">WalletID: {provider.walletID}</Typography>
-                    <Typography variant="body2">Price: {provider.price} OTTC</Typography>
-                    </Box>
-                    <CardActions>
-                    <Button 
-                      size="small" 
-                      variant="contained" 
-                      onClick={() => handleDownloadClick(provider.walletID, provider.price)}
-                    >
-                      Download
-                    </Button>
-                  </CardActions>
+                      <Box display="flex" flexDirection="column" alignItems="flex-start">
+                        <Typography variant="body2">WalletID: {provider.walletID}</Typography>
+                        <Typography variant="body2">Price: {provider.price} OTTC</Typography>
+                      </Box>
+                      <CardActions>
+                        <Button 
+                          size="small" 
+                          variant="contained" 
+                          onClick={() => handleDownload(provider.walletID, provider.price)}
+                        >
+                          Download
+                        </Button>
+                      </CardActions>
                     </Box>
                   </CardContent>
                 </Card>
               </Grid>
             ))}
-        </Grid>
-      </Box>
-
+          </Grid>
         </Box>
-
-        
-
-        <Dialog
-        open={openModal}
-        onClose={handleCloseModal}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <DialogTitle id="alert-dialog-title">{modalTitle}</DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            {modalMessage}
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleCloseModal} autoFocus>
-            OK
-          </Button>
-        </DialogActions>
-      </Dialog>
-
       </Box>
-    );
-  };
 
+      <Modal
+        open={downloadModalOpen}
+        onClose={handleDownloadModalClose}
+        aria-labelledby="download-modal-title"
+        aria-describedby="download-modal-description"
+      >
+        <Box sx={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          width: 400,
+          bgcolor: "background.paper",
+          boxShadow: 24,
+          p: 4,
+          borderRadius: 2,
+        }}>
+          <Typography id="download-modal-title" variant="h6" component="h2">
+            Confirm Download
+          </Typography>
+          <Typography id="download-modal-description" sx={{ mt: 2 }}>
+            {fileDetails && (
+              <>
+                <strong>Name:</strong> {fileDetails.name} <br />
+                <strong>Size:</strong> {fileDetails.size} KB <br />
+                <strong>Type:</strong> {fileDetails.type} <br />
+                <strong>Price:</strong> {selectedPrice} OTTC <br />
+                <strong>Download Location:</strong> {downloadLocation || "Not Selected"}
+              </>
+            )}
+          </Typography>
+          <Box sx={{ display: "flex", justifyContent: "flex-start", mt: 3 }}>
+            <Button onClick={handleSelectDownloadLocation} sx={{ mr: 2 }}>
+              Select Download Location
+            </Button>
+          </Box>
+          <Box sx={{ display: "flex", justifyContent: "flex-end", mt: 3 }}>
+            <Button onClick={handleDownloadModalClose} sx={{ mr: 2 }}>
+              Cancel
+            </Button>
+            <Button
+              onClick={() => handleDownloadClick(selectedWallet, selectedPrice!)}
+              variant="contained"
+              color="primary"
+              disabled={!downloadLocation}
+            >
+              Confirm
+            </Button>
+          </Box>
+        </Box>
+      </Modal>
+    </Box>
+  );
+};
 
 export default Download;
