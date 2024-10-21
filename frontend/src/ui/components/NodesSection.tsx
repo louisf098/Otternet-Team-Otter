ae9e37756503273350a791c19f3979fbaff67cd8
Julian Shin <jsqwertyuiop04@gmail.com>
2024-10-20 21:13:56 -0400

    Call proxy backend route, and do snackbar result output

--- frontend/src/ui/components/NodesSection.tsx
+++ frontend/src/ui/components/NodesSection.tsx
@@ -2,19 +2,64 @@ import React, { useContext } from "react";
 import NodeBox from "./NodeBox";
 import { ProxyContext } from "../contexts/ProxyContext";
 import { proxyNodes } from "../data/proxyNodes";
+import { ProxyData } from "../interfaces/File";
+import { IconButton, Snackbar, SnackbarCloseReason } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
 import "../stylesheets/NodesSection.css";
 
 const NodesSection: React.FC = () => {
   const { selectedNode, setSelectedNode } = useContext(ProxyContext);
+  const [snackbarOpen, setSnackbarOpen] = React.useState(false);
+  const [snackbarMessage, setSnackbarMessage] = React.useState("");
 
   // typeof proxyNodes will need to be changed once we use real data
   const handleSelect = async (node: (typeof proxyNodes)[0]) => {
     try {
       await setSelectedNode(node);
+      const proxyData: ProxyData = {
+        id: node.id,
+        ipAddr: node.ip,
+        price: node.rate,
+        timestamp: new Date().toISOString(),
+      }
+      connectToProxy(proxyData);
     } catch (e) {
       alert(`Failed to connect to ${node.id}`);
     }
   };
+
+  const connectToProxy = async (proxyData: ProxyData) => {
+    try {
+      const response = await fetch("http://localhost:9378/connectToProxy", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(proxyData),
+      });
+      if (response.status === 200) {
+        setSnackbarMessage("Connected to proxy");
+        setSnackbarOpen(true);
+      } else {
+        setSnackbarMessage("Failed to connect to proxy");
+        setSnackbarOpen(true);
+      }
+    } catch (err) {
+      console.error("Error connecting to proxy: ", err);
+      setSnackbarMessage("Error connecting to proxy");
+    }
+  }
+
+  const handleCloseSnackbar = (
+    event: React.SyntheticEvent<any, Event> | Event,
+    reason?: SnackbarCloseReason
+  ) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setSnackbarOpen(false);
+  }
+  
 
   const handleDisconnect = async (node: (typeof proxyNodes)[0]) => {
     if (selectedNode?.id === node.id) {
@@ -33,17 +78,37 @@ const NodesSection: React.FC = () => {
   };
 
   return (
-    <div className="nodes-section">
-      {proxyNodes.sort(sortByRate).map((node) => (
-        <NodeBox
-          key={node.id}
-          node={node}
-          isSelected={selectedNode?.id === node.id}
-          onSelect={handleSelect}
-          onDisconnect={handleDisconnect}
-        />
-      ))}
-    </div>
+    <>
+      <div className="nodes-section">
+        {proxyNodes.sort(sortByRate).map((node) => (
+          <NodeBox
+            key={node.id}
+            node={node}
+            isSelected={selectedNode?.id === node.id}
+            onSelect={handleSelect}
+            onDisconnect={handleDisconnect}
+          />
+        ))}
+      </div>
+      <Snackbar
+        open={snackbarOpen}
+        autoHideDuration={3000}
+        onClose={handleCloseSnackbar}
+        message={snackbarMessage}
+        anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+        action={
+          <IconButton
+            size="small"
+            aria-label="close"
+            color="inherit"
+            onClick={handleCloseSnackbar}
+          >
+            <CloseIcon fontSize="small" />
+          </IconButton>
+        }
+      />
+    </>
+    
   );
 };
 
