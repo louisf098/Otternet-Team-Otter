75e86a0d1ce45208c27be781a9829bfe44628fba
Julian Shin <jsqwertyuiop04@gmail.com>
2024-11-29 16:32:51 -0500

    Refactor dhtnode to be initialized in main of HTTP server

--- backend/server.go
+++ backend/server.go
@@ -1,37 +1,38 @@
 package main
 
 import (
-    "Otternet/backend/api/bitcoin"
-    "Otternet/backend/api/download"
-    files "Otternet/backend/api/files"
-    "Otternet/backend/api/proxy"
-    "context"
-    "encoding/json"
-    "fmt"
-    "log"
-    "net/http"
-    "os"
-    "os/signal"
-    "syscall"
+	"Otternet/backend/api/bitcoin"
+	"Otternet/backend/api/dhtnode"
+	"Otternet/backend/api/download"
+	files "Otternet/backend/api/files"
+	"Otternet/backend/api/proxy"
+	"Otternet/backend/global"
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
-    "github.com/gorilla/handlers"
-    "github.com/gorilla/mux"
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
 )
 
 var corsOptions = handlers.CORS(
-    handlers.AllowedOrigins([]string{"*"}),
-    handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}),
-    handlers.AllowedHeaders([]string{"Content-Type", "Authorization"}),
+	handlers.AllowedOrigins([]string{"*"}),
+	handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}),
+	handlers.AllowedHeaders([]string{"Content-Type", "Authorization"}),
 )
 
 type TestJSON struct {
-    Name string `json:"name"`
+	Name string `json:"name"`
 }
 
 func baseHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello World")
+	fmt.Fprintf(w, "Hello World")
 }
-
 
 /*
 Test Function
@@ -63,50 +64,61 @@ func jsonResponse(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-    r := mux.NewRouter()
-    r.HandleFunc("/test", testOutput)
-    r.HandleFunc("/hello/{name}", nameReader)
-    r.HandleFunc("/json", jsonResponse)
-    r.HandleFunc("/", baseHandler)
+	var err error
+	global.DHTNode, err = dhtnode.CreateLibp2pHost()
+	if err != nil {
+		log.Fatalf("Failed to instantiate the DHT node: %v", err)
+	}
+	global.DHTNode.ConnectToPeer(dhtnode.RelayNodeAddr)
+	global.DHTNode.MakeReservation()
+	global.DHTNode.ConnectToPeer(dhtnode.BootstrapNodeAddr)
 
-    // Register Bitcoin routes
-    r.HandleFunc("/balance", bitcoin.GetBalanceHandler).Methods("GET")
-    r.HandleFunc("/newaddress", bitcoin.GenerateAddressHandler).Methods("GET")
+	defer global.DHTNode.Close()
 
-    // Other existing routes
-    r.HandleFunc("/uploadFile", files.UploadFile).Methods("POST")
-    r.HandleFunc("/deleteFile/{fileHash}", files.DeleteFile).Methods("DELETE")
-    r.HandleFunc("/getUploads", files.GetAllFiles).Methods("GET")
-    r.HandleFunc("/download", download.DownloadFile).Methods("POST")
-    r.HandleFunc("/getDownloadHistory", download.GetDownloadHistory).Methods("GET")
-    r.HandleFunc("/connectToProxy", proxy.ConnectToProxy).Methods("POST")
-    r.HandleFunc("/getProxyHistory", proxy.GetProxyHistory).Methods("GET")
+	r := mux.NewRouter()
+	r.HandleFunc("/test", testOutput)
+	r.HandleFunc("/hello/{name}", nameReader)
+	r.HandleFunc("/json", jsonResponse)
+	r.HandleFunc("/", baseHandler)
 
-    handlerWithCORS := corsOptions(r)
+	// Register Bitcoin routes
+	r.HandleFunc("/balance", bitcoin.GetBalanceHandler).Methods("GET")
+	r.HandleFunc("/newaddress", bitcoin.GenerateAddressHandler).Methods("GET")
 
-    server := &http.Server{
-        Addr:    ":9378",
-        Handler: handlerWithCORS,
-    }
+	// Other existing routes
+	r.HandleFunc("/uploadFile", files.UploadFile).Methods("POST")
+	r.HandleFunc("/deleteFile/{fileHash}", files.DeleteFile).Methods("DELETE")
+	r.HandleFunc("/getUploads", files.GetAllFiles).Methods("GET")
+	r.HandleFunc("/download", download.DownloadFile).Methods("POST")
+	r.HandleFunc("/getDownloadHistory", download.GetDownloadHistory).Methods("GET")
+	r.HandleFunc("/connectToProxy", proxy.ConnectToProxy).Methods("POST")
+	r.HandleFunc("/getProxyHistory", proxy.GetProxyHistory).Methods("GET")
 
-    signalChan := make(chan os.Signal, 1)
-    signal.Notify(signalChan, syscall.SIGTERM, syscall.SIGINT)
-    shutdownComplete := make(chan bool)
-    go func() {
-        println("Preparing to listen on port 9378")
-        if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-            log.Fatalf("ListenAndServer error: %s\n", err)
-        }
-    }()
+	handlerWithCORS := corsOptions(r)
 
-    go func() {
-        sig := <-signalChan
-        fmt.Printf("Received signal: %s\n", sig)
-        if err := server.Shutdown(context.TODO()); err != nil {
-            log.Fatalf("Error during shutdown: %v", err)
-        }
-        shutdownComplete <- true
-    }()
-    <-shutdownComplete
-    log.Println("Server stopped")
+	server := &http.Server{
+		Addr:    ":9378",
+		Handler: handlerWithCORS,
+	}
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, syscall.SIGTERM, syscall.SIGINT)
+	shutdownComplete := make(chan bool)
+	go func() {
+		println("Preparing to listen on port 9378")
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("ListenAndServer error: %s\n", err)
+		}
+	}()
+
+	go func() {
+		sig := <-signalChan
+		fmt.Printf("Received signal: %s\n", sig)
+		if err := server.Shutdown(context.TODO()); err != nil {
+			log.Fatalf("Error during shutdown: %v", err)
+		}
+		shutdownComplete <- true
+	}()
+	<-shutdownComplete
+	log.Println("Server stopped")
 }
