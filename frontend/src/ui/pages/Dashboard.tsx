2bb35ca41d4278e6077aa1d0c03e17533bc11cbb
eethanlau <ethan.lau151@gmail.com>
2024-12-10 01:03:35 -0500

    Complete transaction history table

--- frontend/src/ui/pages/Dashboard.tsx
+++ frontend/src/ui/pages/Dashboard.tsx
@@ -18,7 +18,7 @@ import Tooltip from "@mui/material/Tooltip";
 import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
 import { AuthContext } from "../contexts/AuthContext";
 import { getBalance, getTransactions } from "../apis/bitcoin-core";
-
+import DownloadHistoryTable from "../components/DownloadHistoryTable";
 interface TransactionData {
   transactionID: string;
   dateTime: number;
@@ -87,13 +87,8 @@ const Dashboard: React.FC<DashboardProps> = () => {
     setBalance(fetchedBalance);
   };
 
-  const fetchTransactions = async () => {
-    let transactions = getTransactions(walletName);
-  };
-
   useEffect(() => {
     fetchBalance();
-    fetchTransactions();
   }, []);
 
   return (
@@ -221,8 +216,9 @@ const Dashboard: React.FC<DashboardProps> = () => {
             }
             id="dashboard-tab-upload"
           />
-          <Tab label="Download History" id="dashboard-tab-history" />
+          <Tab label="Download History" id="dashboard-tab-download-history" />
           <Tab label="Proxy History" id="dashboard-tab-proxy" />
+          <Tab label="Transaction History" id="dashboard-tab-transaction-history" />
         </Tabs>
 
         <TabSelector value={selectedTableTab} index={0}>
@@ -233,17 +229,25 @@ const Dashboard: React.FC<DashboardProps> = () => {
           />
         </TabSelector>
         <TabSelector value={selectedTableTab} index={1}>
-          <TransactionHistoryTable
+          <DownloadHistoryTable
             setSnackbarOpen={setSnackbarOpen}
             setSnackbarMessage={setSnackbarMessage}
             handleCopy={handleCopy}
           />
-        </TabSelector>
+          </TabSelector>
         <TabSelector value={selectedTableTab} index={2}>
           <ProxyHistoryTable
             setSnackbarOpen={setSnackbarOpen}
             setSnackbarMessage={setSnackbarMessage}
             handleCopy={handleCopy}
+          />
+        </TabSelector>
+        <TabSelector value={selectedTableTab} index={3}>
+          <TransactionHistoryTable
+            setSnackbarOpen={setSnackbarOpen}
+            setSnackbarMessage={setSnackbarMessage}
+            handleCopy={handleCopy}
+            walletName={walletName}
           />
         </TabSelector>
       </Box>
