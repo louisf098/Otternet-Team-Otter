139ba6e0e867ba918d6f91a4c892eef05910f47d
Daniel Leong <daniel.leong@stonybrook.edu>
2024-12-11 21:53:25 -0500

    modified proxy history table

--- frontend/src/ui/components/ProxyHistoryTable.tsx
+++ frontend/src/ui/components/ProxyHistoryTable.tsx
@@ -1,3 +1,109 @@
+// import React, { useEffect } from "react";
+// import Table from "@mui/material/Table";
+// import TableBody from "@mui/material/TableBody";
+// import TableCell from "@mui/material/TableCell";
+// import TableContainer from "@mui/material/TableContainer";
+// import TableHead from "@mui/material/TableHead";
+// import TableRow from "@mui/material/TableRow";
+// import Paper from "@mui/material/Paper";
+// import { ProxyData } from "../interfaces/File";
+// import { Tooltip } from "@mui/material";
+// import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
+// import { AuthContext } from "../contexts/AuthContext";
+
+// interface proxyHistoryTableProps {
+//     setSnackbarOpen: (open: boolean) => void;
+//     setSnackbarMessage: (message: string) => void;
+//     handleCopy: (text: string) => void;
+// }
+
+// const ProxyHistoryTable: React.FC<proxyHistoryTableProps> = ({ setSnackbarOpen, setSnackbarMessage, handleCopy }) => {
+//   const [proxies, setProxies] = React.useState<ProxyData[]>([]);
+//   const { publicKey } = React.useContext(AuthContext);
+
+//   useEffect(() => {
+//     fetchProxyData();
+//   }, [])
+
+//   const fetchProxyData = async () => {
+//     try {
+//       const response = await fetch(`http://localhost:9378/getProxyHistory/${publicKey}`, {
+//         method: "GET",
+//         headers: {
+//           "Content-Type": "application/json"
+//         }
+//       });
+//       if (response.status === 404) {
+//         setSnackbarMessage("No proxy history found");
+//         setSnackbarOpen(true);
+//         return;
+//       }
+//       const data = await response.json();
+//       setProxies(data);
+//     } catch (err) {
+//       console.error("Error fetching proxy data: ", err);
+//       setSnackbarMessage("Error fetching proxy data");
+//       setSnackbarOpen(true);
+//     }
+//   }
+
+//   return (
+//     <TableContainer component={Paper} sx={{ mt: 1 }}>
+//       <Table sx={{ minWidth: 500 }} aria-label="simple table">
+//         <TableHead>
+//           <TableRow>
+//             <TableCell>Timestamp</TableCell>
+//             <TableCell>Proxy IP Address</TableCell>
+//             <TableCell>Rate (OTTC/KB)</TableCell>
+//             <TableCell>
+//               Proxy Wallet ID
+//               <Tooltip title="Click Wallet ID to copy to your clipboard" arrow>
+//                 <HelpOutlineIcon sx={{ fontSize: 16, paddingLeft: 1}}/>
+//               </Tooltip>
+//             </TableCell>
+//           </TableRow>
+//         </TableHead>
+//         <TableBody>
+//           {proxies.length === 0 ? (
+//             <TableRow>
+//               <TableCell colSpan={4} align="center">
+//                 You have not used any proxies yet.
+//               </TableCell>
+//             </TableRow>
+//           ) :
+
+//           proxies.map((proxy) => (
+//             <TableRow
+//               key={proxy.timestamp}
+//               sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+//             >
+//               <TableCell component="th" scope="row">
+//                 {proxy.timestamp}
+//               </TableCell>
+//               <TableCell>{proxy.ipAddr}</TableCell>
+//               <TableCell>{proxy.price}</TableCell>
+//               <TableCell
+//                 onClick={() => handleCopy(proxy.srcID)}
+//                 sx={{
+//                   maxWidth: "200px",
+//                   wordWrap: "break-word",
+//                   whiteSpace: "normal",
+//                   cursor: "pointer",
+//                 }}
+//               >
+//                 <Tooltip title="Click to copy" arrow>
+//                   <span>{proxy.srcID}</span>
+//                 </Tooltip>
+//               </TableCell>
+//             </TableRow>
+//           ))}
+//         </TableBody>
+//       </Table>
+//     </TableContainer>
+//   );
+// };
+
+// export default ProxyHistoryTable;
 import React, { useEffect } from "react";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
@@ -6,32 +112,34 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import { ProxyData } from "../interfaces/File";
+import { ProxyHistory } from "../interfaces/File";
 import { Tooltip } from "@mui/material";
 import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
-import { AuthContext } from "../contexts/AuthContext";
 
 interface proxyHistoryTableProps {
-    setSnackbarOpen: (open: boolean) => void;
-    setSnackbarMessage: (message: string) => void;
-    handleCopy: (text: string) => void;
+  setSnackbarOpen: (open: boolean) => void;
+  setSnackbarMessage: (message: string) => void;
+  handleCopy: (text: string) => void;
 }
 
-const ProxyHistoryTable: React.FC<proxyHistoryTableProps> = ({ setSnackbarOpen, setSnackbarMessage, handleCopy }) => {
-  const [proxies, setProxies] = React.useState<ProxyData[]>([]);
-  const { publicKey } = React.useContext(AuthContext);
+const ProxyHistoryTable: React.FC<proxyHistoryTableProps> = ({
+  setSnackbarOpen,
+  setSnackbarMessage,
+  handleCopy,
+}) => {
+  const [proxies, setProxies] = React.useState<ProxyHistory[]>([]);
 
   useEffect(() => {
     fetchProxyData();
-  }, [])
+  }, []);
 
   const fetchProxyData = async () => {
     try {
-      const response = await fetch(`http://localhost:9378/getProxyHistory/${publicKey}`, {
+      const response = await fetch("http://localhost:9378/getProxyHistory", {
         method: "GET",
         headers: {
-          "Content-Type": "application/json"
-        }
+          "Content-Type": "application/json",
+        },
       });
       if (response.status === 404) {
         setSnackbarMessage("No proxy history found");
@@ -39,26 +147,40 @@ const ProxyHistoryTable: React.FC<proxyHistoryTableProps> = ({ setSnackbarOpen, 
         return;
       }
       const data = await response.json();
+      // console.log(data);
       setProxies(data);
     } catch (err) {
       console.error("Error fetching proxy data: ", err);
       setSnackbarMessage("Error fetching proxy data");
       setSnackbarOpen(true);
     }
-  }
+  };
+
+  const formatDate = (dateString: string) => {
+    const date = new Date(dateString);
+    return date.toLocaleString("en-US", {
+      weekday: "short",
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+    });
+  };
 
   return (
     <TableContainer component={Paper} sx={{ mt: 1 }}>
       <Table sx={{ minWidth: 500 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Timestamp</TableCell>
+            <TableCell>Start Time</TableCell>
+            <TableCell>End Time</TableCell>
             <TableCell>Proxy IP Address</TableCell>
             <TableCell>Rate (OTTC/KB)</TableCell>
             <TableCell>
               Proxy Wallet ID
               <Tooltip title="Click Wallet ID to copy to your clipboard" arrow>
-                <HelpOutlineIcon sx={{ fontSize: 16, paddingLeft: 1}}/>
+                <HelpOutlineIcon sx={{ fontSize: 16, paddingLeft: 1 }} />
               </Tooltip>
             </TableCell>
           </TableRow>
@@ -70,33 +192,39 @@ const ProxyHistoryTable: React.FC<proxyHistoryTableProps> = ({ setSnackbarOpen, 
                 You have not used any proxies yet.
               </TableCell>
             </TableRow>
-          ) : 
-          
-          proxies.map((proxy) => (
-            <TableRow
-              key={proxy.timestamp}
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {proxy.timestamp}
-              </TableCell>
-              <TableCell>{proxy.ipAddr}</TableCell>
-              <TableCell>{proxy.price}</TableCell>
-              <TableCell
-                onClick={() => handleCopy(proxy.srcID)}
-                sx={{
-                  maxWidth: "200px",
-                  wordWrap: "break-word",
-                  whiteSpace: "normal",
-                  cursor: "pointer",
-                }}
-              >
-                <Tooltip title="Click to copy" arrow>
-                  <span>{proxy.srcID}</span>
-                </Tooltip>
-              </TableCell>
-            </TableRow>
-          ))}
+          ) : (
+            proxies.map((proxy) => {
+              // Log the proxy data
+              console.log("Proxy Data:", proxy);
+
+              return (
+                <TableRow
+                  key={proxy.ConnectTime}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {formatDate(proxy.ConnectTime)}
+                  </TableCell>
+                  <TableCell>{formatDate(proxy.DisconnectTime)}</TableCell>
+                  <TableCell>{proxy.IPAddr}</TableCell>
+                  <TableCell>{proxy.Price}</TableCell>
+                  <TableCell
+                    onClick={() => handleCopy(proxy.ProxyWalletID)}
+                    sx={{
+                      maxWidth: "200px",
+                      wordWrap: "break-word",
+                      whiteSpace: "normal",
+                      cursor: "pointer",
+                    }}
+                  >
+                    <Tooltip title="Click to copy" arrow>
+                      <span>{proxy.ProxyWalletID}</span>
+                    </Tooltip>
+                  </TableCell>
+                </TableRow>
+              );
+            })
+          )}
         </TableBody>
       </Table>
     </TableContainer>
